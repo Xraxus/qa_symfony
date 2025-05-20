13021074ed1a41aa67fa377043278e14b75ddbe5
xraxus <xraxus@gmail.com>
2025-05-20 23:08:57 +0200

    Add question pagination

--- app/src/Controller/QuestionController.php
+++ app/src/Controller/QuestionController.php
@@ -6,26 +6,37 @@ namespace App\Controller;
 
 use App\Entity\Question;
 use App\Repository\QuestionRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController extends AbstractController
 {
     #[Route('/question', name: 'question_list')]
-    public function list(QuestionRepository $questionRepository): Response
-    {
-        $questions = $questionRepository->findAll();
+    public function list(
+        QuestionRepository $questionRepository,
+        PaginatorInterface $paginator,
+        Request $request,
+    ): Response {
+        $queryBuilder = $questionRepository->createQueryBuilder('q')
+            ->orderBy('q.createdAt', 'DESC');
+
+        $pagination = $paginator->paginate(
+            $queryBuilder, /* zapytanie lub QueryBuilder */
+            $request->query->getInt('page', 1), /* numer strony, domyślnie 1 */
+            10 /* limit elementów na stronę */
+        );
 
         return $this->render('question/list.html.twig', [
-            'questions' => $questions,
+            'pagination' => $pagination,
         ]);
     }
 
     #[Route('/question/{id}', name: 'question_show')]
     public function show(Question $question): Response
     {
-        // $question jest już wczytany z bazy
         return $this->render('question/show.html.twig', [
             'question' => $question,
         ]);
