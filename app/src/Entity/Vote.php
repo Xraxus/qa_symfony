5d6ddaa21dbfa80a5ba7db0aa7875de9fdd93d48
xraxus <xraxus@gmail.com>
2025-05-22 13:25:19 +0200

    Add Vote Entity

---
+++ app/src/Entity/Vote.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoteRepository::class)]
+#[ORM\Table(name: 'votes')]
+class Vote
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    // Głos może dotyczyć odpowiedzi
+    #[ORM\ManyToOne(inversedBy: 'votes')]
+    private ?Answer $answer = null;
+
+    // Głos może dotyczyć komentarza
+    #[ORM\ManyToOne(inversedBy: 'votes')]
+    private ?Comment $comment = null;
+
+    #[ORM\ManyToOne(inversedBy: 'votes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    // Wartość głosu: +1 lub -1
+    #[ORM\Column]
+    private int $value;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAnswer(): ?Answer
+    {
+        return $this->answer;
+    }
+
+    public function setAnswer(?Answer $answer): static
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    public function getComment(): ?Comment
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?Comment $comment): static
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): static
+    {
+        if (!in_array($value, [-1, 1], true)) {
+            throw new \InvalidArgumentException("Wartość głosu musi być +1 lub -1");
+        }
+
+        $this->value = $value;
+
+        return $this;
+    }
+}
